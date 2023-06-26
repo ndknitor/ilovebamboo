2a855ac30a639f0196eb07229d10ad24e46cc759
ndknitor <nguyentuanpkn@gmail.com>
2023-06-26 15:44:00 +0700

    Test pwa

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'Love Ruby';
+const urlsToCache = [
+    '/index.html',
+    '/css/*',
+    '/fonts/*',
+    '/images/*',
+    '/js/*'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Cache opened');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
