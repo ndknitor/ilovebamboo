a882b00afa4673939ddbbbb5ce10962629a6b529
ndknitor <nguyentuanpkn@gmail.com>
2023-07-01 11:19:14 +0700

    Test offline mode

--- service-worker.js
+++ service-worker.js
@@ -7,24 +7,42 @@ const urlsToCache = [
     '/js/*'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
+    // Perform installation: caching app shell and assets
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then((cache) => {
+            .then(cache => {
                 console.log('Cache opened');
                 return cache.addAll(urlsToCache);
             })
     );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('activate', event => {
+    // Clean up old caches if any exist
+    event.waitUntil(
+        caches.keys()
+            .then(cacheNames => {
+                return Promise.all(
+                    cacheNames.filter(cacheName => {
+                        return cacheName !== CACHE_NAME;
+                    }).map(cacheName => {
+                        return caches.delete(cacheName);
+                    })
+                );
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then((response) => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
+            .then(response => {
+                // Try to fetch the request from the network if it's not in the cache
+                return response || fetch(event.request);
+            })
+            .catch(error => {
+                console.error('Error in fetch handler:', error);
             })
     );
 });
